71a52ceffe660b73cc7de3ce2924d7c82b131500
artem <akaplun@kse.org.ua>
2024-05-17 11:37:07 +0300

    PA-1

---
+++ main.py
@@ -0,0 +1,79 @@
+
+# task 1
+# x = int(input("Enter a number for x: "))
+# y = int(input("Enter a number for y: "))
+# print(f"{x + y}")
+# print(f"{x - y}")
+# print(f"{x * y}")
+# print(f"{x / y}")
+# print(f"{x % y}")
+
+# task 2
+# Celsius = float(input("enter a number for Celsius: "))
+# print(f"F={Celsius * (9/5) + 32}")
+# task 3
+# input_string = input("Enter a string: ")
+# print(f"len of string {len(input_string)}")
+# print(f"{input_string[0]}")
+# print(f"{input_string[-1]}")
+# print(f"{input_string[::-1]}")
+
+# tesk 4
+# user_input = int(input("Write a number"))
+# if user_input % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# task 5
+# Principal_amount = int(input("Enter the principal amount:"))
+# Time_period_in_years = int(input("Enter the time period in years:"))
+# Rate_of_interest = float(input("Enter the rate of interest:"))
+# print(f"SI={(Rate_of_interest*Principal_amount*Time_period_in_years)/100}")
+
+# task 6
+# base = int(input("Enter the base number: "))
+# height = int(input("Entre the height number: "))
+# print(f"Area={(1/2)*base*height}")
+
+# task 7
+# weight = float(input("Enter your weight in kg: "))
+# height = float(input("Enter your height in meters: "))
+# print(f"BMI formula = {weight/(height*height):.2f}")
+
+# task 8
+# mark_1 = int(input("Enter a number: "))
+# mark_2 = int(input("Enter second number: "))
+# mark_3 = int(input("Enter a third number: "))
+# avg = (mark_1 + mark_2 + mark_3) / 3
+# if 90<=avg<=100:
+#     print(f"average:{avg:.2f}, grade: A")
+# elif 80<=avg<=89:
+#     print(f"average:{avg:.2f}, grade: B")
+# elif 70<=avg<=79:
+#     print(f"average:{avg:.2f}, grade: C")
+# elif 60<=avg<=69:
+#     print(f"average:{avg:.2f}, grade: D")
+# else:
+#     print(f"average:{avg:.2f}, grade: F")
+
+# task 9
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# number3 = int(input("Enter third number: "))
+# if number1 > number2 and number1 > number3:
+#     print(number1)
+# elif number2 > number1 and number2 > number3:
+#     print(number2)
+# else:
+#     print(number3)
+
+# task 10
+# number1 = int(input("Enter a number: "))
+# number2 = int(input("Enter another number: "))
+# temp = number1
+# number1 = number2
+# number2 = temp
+# print(number1, number2)
+
+
